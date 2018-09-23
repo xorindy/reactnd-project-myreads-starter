1b13bfe10ec877bf780bc32ab624f3ce9d0e0654
Clarinda Bui <clarindabui@outlook.com>
2018-09-22 23:52:35 -0700

    Fix searched query

---
+++ src/Book.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+class Book extends Component {
+  render() {
+    let displayThumbnail = this.props.imageLinks ? this.props.imageLinks.thumbnail : '';
+    
+  	return (
+    	<div className="book">
+        	<div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${displayThumbnail}")` }}></div>
+            <div className="book-shelf-changer">
+            	<select
+					      onChange={(event) => this.props.changeShelf(
+                  this.props.book, event.target.value
+                  )
+                }                      
+					      value={this.props.book.shelf}
+				      >
+
+                <option value="move" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="remove">Remove</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title"> {this.props.book.title} </div>
+          <div className="book-authors"> {this.props.book.authors} </div>
+		  </div>
+    )
+  }
+}
+
+export default Book
