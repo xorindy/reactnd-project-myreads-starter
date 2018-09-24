2ddfebe462342bd8c416856621d1919a68c3fb28
Clarinda Bui <clarindabui@outlook.com>
2018-09-23 17:06:17 -0700

    Add comments

--- src/components/Book.js
+++ src/components/Book.js
@@ -8,6 +8,10 @@ class Book extends React.Component {
         	<div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : "No Image" } ")` }}></div>
             <div className="book-shelf-changer">
+              {
+                //Default value for options will the the current shelf the book is in.
+                //If the book is not on a shelf, it will set to None.
+              }
             	<select value={this.props.book.shelf || "none"}
                   onChange={ (e) => {this.props.changeShelf(this.props.book, e.target.value)} }>
 
@@ -16,7 +20,7 @@ class Book extends React.Component {
                 <option value="wantToRead">Want to Read</option>
                 <option value="read">Read</option>
                 <option value="none">None</option>
-                
+
               </select>
             </div>
           </div>
